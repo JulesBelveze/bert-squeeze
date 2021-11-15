515c4fb02836f5ca8baf83dc574c9768eb17a25f
JulesBelveze <jules.belveze@hotmail.fr>
2021-11-15 08:25:20 +0100

    refacto: remove RomBert

--- bert-squeeze/models/__init__.py
+++ bert-squeeze/models/__init__.py
@@ -3,4 +3,3 @@ from .lt_bert import LtCustomBert
 from .lt_distilbert import LtCustomDistilBert
 from .lr import BowLogisticRegression
 from .exit_berts.deebert import DeeBert
-from .exit_berts.romebert import RomeBert
