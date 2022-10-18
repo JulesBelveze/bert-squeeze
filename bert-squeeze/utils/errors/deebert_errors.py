e80a97a5040a367c6503176a2f008f7a1ccf57fa
JulesBelveze <jules.belveze@hotmail.fr>
2022-10-18 07:17:06 +0200

    documentation: errors

--- bert-squeeze/utils/errors/deebert_errors.py
+++ bert-squeeze/utils/errors/deebert_errors.py
@@ -1,8 +1,14 @@
 class RampException(Exception):
     """
     This error is only used to exit the encoder stack earlier.
+
+    Args:
+        message (str):
+            error message
+        exit_layer (int):
+            index of the exit layer
     """
 
-    def __init__(self, message, exit_layer):
+    def __init__(self, message: str, exit_layer: int):
         self.message = message
         self.exit_layer = exit_layer
