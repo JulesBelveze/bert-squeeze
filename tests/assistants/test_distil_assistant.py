4e09dd29185fd9b4e5e0fc05b998f7dad9ccc5c0
JulesBelveze <jules.belveze@hotmail.fr>
2023-05-12 17:17:54 +0200

    tests: fix and update tests

--- tests/assistants/test_distil_assistant.py
+++ tests/assistants/test_distil_assistant.py
@@ -14,7 +14,7 @@ class TestDistilAssistant:
         distil_assistant = DistilAssistant(
             "distil",
             data_kwargs={"path": "emotion"},
-            general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6}
+            general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
         )
         assert distil_assistant.teacher is None
         assert distil_assistant.student is None
@@ -32,8 +32,9 @@ class TestDistilAssistant:
             data_kwargs={"path": "emotion"},
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             student_kwargs={
-                "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+                "_target_": "transformers.models.auto.AutoModelForSequenceClassification.from_pretrained",
+                "pretrained_model_name_or_path": "microsoft/xtremedistil-l6-h256-uncased",
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -51,8 +52,9 @@ class TestDistilAssistant:
             data_kwargs={"path": "emotion"},
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             teacher_kwargs={
-                "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+                "_target_": "transformers.models.auto.AutoModelForSequenceClassification.from_pretrained",
+                "pretrained_model_name_or_path": "microsoft/xtremedistil-l6-h256-uncased",
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -71,11 +73,11 @@ class TestDistilAssistant:
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -93,16 +95,17 @@ class TestDistilAssistant:
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
             },
             data_kwargs={
-                "path": "emotion",
+                "is_local": False,
+                "path": "SetFit/emotion",
                 "train_batch_size": 16,
                 "eval_batch_size": 4
-            }
+            },
         )
         assert isinstance(distil_assistant.data.train_dataloader(), DataLoader)
         assert len(distil_assistant.data.train_dataloader()) == 1000
@@ -116,7 +119,6 @@ class TestDistilSoftAssistant:
         """"""
         distil_assistant = DistilAssistant(
             "distil-soft",
-
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             data_kwargs={
                 "path": "emotion",
@@ -124,9 +126,9 @@ class TestDistilSoftAssistant:
                     "is_local": False,
                     "text_col": "text",
                     "split": "train",
-                    "path": "SetFit/emotion"
-                }
-            }
+                    "path": "SetFit/emotion",
+                },
+            },
         )
         assert distil_assistant.teacher is None
         assert distil_assistant.student is None
@@ -148,12 +150,12 @@ class TestDistilSoftAssistant:
                     "is_local": False,
                     "text_col": "text",
                     "split": "train",
-                    "path": "SetFit/emotion"
-                }
+                    "path": "SetFit/emotion",
+                },
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -175,12 +177,12 @@ class TestDistilSoftAssistant:
                     "is_local": False,
                     "text_col": "text",
                     "split": "train",
-                    "path": "SetFit/emotion"
-                }
+                    "path": "SetFit/emotion",
+                },
             },
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -202,16 +204,16 @@ class TestDistilSoftAssistant:
                     "is_local": False,
                     "text_col": "text",
                     "split": "train",
-                    "path": "SetFit/emotion"
-                }
+                    "path": "SetFit/emotion",
+                },
             },
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -229,22 +231,23 @@ class TestDistilSoftAssistant:
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
             },
             data_kwargs={
-                "path": "emotion",
+                "is_local": False,
+                "path": "SetFit/emotion",
                 "soft_data_config": {
                     "is_local": False,
                     "text_col": "text",
-                    "split": "train",
-                    "path": "SetFit/emotion"
+                    "split": "train[:10%]",
+                    "path": "SetFit/emotion",
                 },
                 "train_batch_size": 16,
-                "eval_batch_size": 4
-            }
+                "eval_batch_size": 4,
+            },
         )
         assert isinstance(distil_assistant.data.train_dataloader(), DataLoader)
         assert len(distil_assistant.data.train_dataloader()) == 1625
@@ -266,11 +269,11 @@ class TestDistilHardAssistant:
                     "split": "split",
                     "text_col": "text",
                     "path": "SetFit/emotion",
-                    "max_samples": 10
+                    "max_samples": 10,
                 },
                 "train_batch_size": 2,
-                "eval_batch_size": 2
-            }
+                "eval_batch_size": 2,
+            },
         )
         assert distil_assistant.teacher is None
         assert distil_assistant.student is None
@@ -288,14 +291,11 @@ class TestDistilHardAssistant:
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             data_kwargs={
                 "path": "emotion",
-                "soft_data_config": {
-                    "is_local": False,
-                    "path": "SetFit/emotion"
-                }
+                "soft_data_config": {"is_local": False, "path": "SetFit/emotion"},
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -313,14 +313,11 @@ class TestDistilHardAssistant:
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             data_kwargs={
                 "path": "emotion",
-                "soft_data_config": {
-                    "is_local": False,
-                    "path": "SetFit/emotion"
-                }
+                "soft_data_config": {"is_local": False, "path": "SetFit/emotion"},
             },
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -338,18 +335,15 @@ class TestDistilHardAssistant:
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             data_kwargs={
                 "path": "emotion",
-                "soft_data_config": {
-                    "is_local": False,
-                    "path": "SetFit/emotion"
-                }
+                "soft_data_config": {"is_local": False, "path": "SetFit/emotion"},
             },
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -366,20 +360,20 @@ class TestDistilHardAssistant:
             "distil-hard",
             general_kwargs={"labels": [0, 1, 2, 3, 4, 5], "num_labels": 6},
             data_kwargs={
-                "path": "emotion",
-                "split": "split",
+                "path": "SetFit/emotion",
+                "split": "train",
                 "hard_labeler": {
                     "dataset_config": {
                         "is_local": False,
-                        "split": "train",
+                        "split": "train[:10%]",
                         "text_col": "text",
                         "path": "SetFit/emotion",
-                        "max_samples": 100
+                        "max_samples": 100,
                     }
                 },
                 "train_batch_size": 2,
-                "eval_batch_size": 4
-            }
+                "eval_batch_size": 4,
+            },
         )
 
         assert isinstance(distil_assistant.data.train_dataloader(), DataLoader)
@@ -395,9 +389,11 @@ class TestDistilAssistantParallel:
         distil_assistant = DistilAssistant(
             "distil-parallel",
             data_kwargs={
-                "path": resource_filename("bert_squeeze", "data/local_datasets/parallel_dataset.py"),
-                "is_local": True
-            }
+                "path": resource_filename(
+                    "bert_squeeze", "data/local_datasets/parallel_dataset.py"
+                ),
+                "is_local": True,
+            },
         )
         assert distil_assistant.teacher is None
         assert distil_assistant.student is None
@@ -413,12 +409,14 @@ class TestDistilAssistantParallel:
         distil_assistant = DistilAssistant(
             "distil-parallel",
             data_kwargs={
-                "path": resource_filename("bert_squeeze", "data/local_datasets/parallel_dataset.py"),
-                "is_local": True
+                "path": resource_filename(
+                    "bert_squeeze", "data/local_datasets/parallel_dataset.py"
+                ),
+                "is_local": True,
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -434,12 +432,14 @@ class TestDistilAssistantParallel:
         distil_assistant = DistilAssistant(
             "distil-parallel",
             data_kwargs={
-                "path": resource_filename("bert_squeeze", "data/local_datasets/parallel_dataset.py"),
-                "is_local": True
+                "path": resource_filename(
+                    "bert_squeeze", "data/local_datasets/parallel_dataset.py"
+                ),
+                "is_local": True,
             },
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -455,16 +455,18 @@ class TestDistilAssistantParallel:
         distil_assistant = DistilAssistant(
             "distil-parallel",
             data_kwargs={
-                "path": resource_filename("bert_squeeze", "data/local_datasets/parallel_dataset.py"),
-                "is_local": True
+                "path": resource_filename(
+                    "bert_squeeze", "data/local_datasets/parallel_dataset.py"
+                ),
+                "is_local": True,
             },
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-            }
+            },
         )
 
         assert distil_assistant.teacher is None
@@ -481,18 +483,20 @@ class TestDistilAssistantParallel:
             "distil-parallel",
             teacher_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
-                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin"
+                "checkpoints": "../tests/fixtures/resources/lr_dummy.bin",
             },
             student_kwargs={
                 "_target_": "tests.fixtures.dummy_models.Lr",
             },
             data_kwargs={
-                "path": resource_filename("bert_squeeze", "data/local_datasets/parallel_dataset.py"),
+                "path": resource_filename(
+                    "bert_squeeze", "data/local_datasets/parallel_dataset.py"
+                ),
                 "is_local": True,
                 "train_batch_size": 16,
-                "eval_batch_size": 4
-            }
+                "eval_batch_size": 4,
+            },
         )
         assert isinstance(distil_assistant.data.train_dataloader(), DataLoader)
-        assert len(distil_assistant.data.train_dataloader()) == 6
-        assert len(distil_assistant.data.val_dataloader()) == 25
+        assert len(distil_assistant.data.train_dataloader()) == 187
+        assert len(distil_assistant.data.val_dataloader()) == 125
