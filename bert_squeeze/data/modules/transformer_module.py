3f82a99f1a442aad953af4bf79db1b49998c2d1d
JulesBelveze <jules.belveze@hotmail.fr>
2024-01-29 11:19:25 +0100

    [bert_squeeze] - feature: add Seq2SeqTransformerDataModule for sequence-to-sequence tasks
     - Define Seq2SeqTransformerDataModule class with dataset and tokenizer handling
     - Include featurization and setup methods for customized data processing
     - Implement train, test, and validation data loaders for model training and evaluation

--- bert_squeeze/data/modules/transformer_module.py
+++ bert_squeeze/data/modules/transformer_module.py
@@ -1,8 +1,6 @@
-import logging
 from typing import Optional
 
 import datasets
-import lightning.pytorch as pl
 from omegaconf import DictConfig
 from overrides import overrides
 from torch.utils.data import DataLoader
@@ -180,3 +178,119 @@ class TransformerParallelDataModule(TransformerDataModule):
 
         tokenized_dataset.set_format(type='torch', columns=columns)
         return tokenized_dataset
+
+
+class Seq2SeqTransformerDataModule(BaseDataModule):
+    """
+    DataModule for Transformer-based models on sequence-to-sequence tasks.
+
+    Args:
+        dataset_config (HydraConfig):
+            dataset configuration
+        tokenizer_name (str):
+            name of the pre-trained tokenizer to use
+        max_target_length (int):
+            maximum sequence length of the targeted text
+       max_source_length (int):
+            maximum sequence length of the source text
+    """
+
+    def __init__(
+        self,
+        dataset_config: DictConfig,
+        tokenizer_name: str,
+        max_target_length: int,
+        max_source_length: int,
+        **kwargs,
+    ):
+        super().__init__()
+        self.dataset_config = dataset_config
+        self.source_col = dataset_config.source_col
+        self.target_col = dataset_config.target_col
+
+        self.max_target_length = max_target_length
+        self.max_source_length = max_source_length
+
+        self.train_batch_size = kwargs.get("train_batch_size", 32)
+        self.eval_batch_size = kwargs.get("eval_batch_size", 32)
+
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+
+        self.dataset = None
+        self.train = None
+        self.test = None
+        self.val = None
+
+    def featurize(self) -> datasets.DatasetDict:
+        """
+        Returns:
+            DatasetDict: featurized dataset
+        """
+        tokenized_dataset = self.dataset.map(
+            lambda x: self.tokenizer(
+                x[self.source_col],
+                padding="longest",
+                max_length=self.max_source_length,
+                truncation=True,
+            )
+        )
+        with self.tokenizer.as_target_tokenizer():
+            tokenized_dataset = tokenized_dataset.map(
+                lambda x: {
+                    "labels": self.tokenizer(
+                        x[self.target_col],
+                        padding="longest",
+                        max_length=self.max_target_length,
+                        truncation=True,
+                    )["input_ids"]
+                }
+            )
+        columns = ["input_ids", "attention_mask", "labels"]
+        if "distilbert" not in self.tokenizer.name_or_path:
+            columns += ["token_type_ids"]
+
+        tokenized_dataset.set_format(type='torch', columns=columns)
+        return tokenized_dataset
+
+    def setup(self, stage: Optional[str] = None):
+        """"""
+        featurized_dataset = self.featurize()
+        self.train = featurized_dataset["train"]
+        self.val = featurized_dataset["validation"]
+        self.test = featurized_dataset["test"]
+
+    def train_dataloader(self) -> DataLoader:
+        """
+        Returns:
+            DataLoader: train dataloader
+        """
+        return DataLoader(
+            self.train,
+            # collate_fn=self._collate_fn(),
+            batch_size=self.train_batch_size,
+            drop_last=True,
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        """
+        Returns:
+            DataLoader: test dataloader
+        """
+        return DataLoader(
+            self.test,
+            # collate_fn=self._collate_fn(),
+            batch_size=self.eval_batch_size,
+            drop_last=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        """
+        Returns:
+            DataLoader: Validation dataloader
+        """
+        return DataLoader(
+            self.val,
+            # collate_fn=self._collate_fn(),
+            batch_size=self.eval_batch_size,
+            drop_last=True,
+        )
