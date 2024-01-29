94c9b4d5cba27c77d247c7c61d901656de0889b7
JulesBelveze <jules.belveze@hotmail.fr>
2024-01-29 11:18:43 +0100

    [bert_squeeze] - refactor: separate base classes for Transformer modules
     - Move sequence classification specific methods to a new subclass
     - Add NotImplementedError() for base class methods
     - Improve code structure and maintainability

--- bert_squeeze/models/base_lt_module.py
+++ bert_squeeze/models/base_lt_module.py
@@ -26,36 +26,21 @@ class BaseTransformerModule(pl.LightningModule):
     Args:
         training_config (DictConfig):
             training configuration
-        num_labels (int):
-            number of labels
         pretrained_model (str):
             name of the pretrained Transformer model to use
     """
 
-    def __init__(
-        self,
-        training_config: DictConfig,
-        num_labels: int,
-        pretrained_model: str = None,
-        **kwargs,
-    ):
-        super(BaseTransformerModule, self).__init__()
-        self._sanity_checks(training_config)
+    def __init__(self, training_config: DictConfig, pretrained_model: str, **kwargs):
+        super().__init__()
 
         self.config = training_config
-        self.num_labels = num_labels
 
         self.pretrained_model = pretrained_model
-        self.model_config = AutoConfig.from_pretrained(
-            pretrained_model, num_labels=num_labels
-        )
+        self.model_config = None
 
         self.training_step_outputs = []
         self.test_step_outputs = []
         self.validation_step_outputs = []
-
-        self._set_scorers(kwargs.get("scorer_type", "regular"))
-        self._set_objective()
 
     def forward(self, **kwargs):
         """"""
@@ -76,16 +61,7 @@ class BaseTransformerModule(pl.LightningModule):
 
     def on_validation_epoch_end(self):
         """"""
-        if not self.trainer.sanity_checking:
-            all_logits = torch.cat(
-                [pred["logits"] for pred in self.validation_step_outputs]
-            )
-            all_probs = F.softmax(all_logits, dim=-1)
-            labels_probs = all_probs.numpy()
-            self.log_eval_report(labels_probs)
-
-        self.valid_scorer.reset()
-        self.validation_step_outputs.clear()
+        raise NotImplementedError()
 
     def test_step(self, batch, batch_idx, *args, **kwargs):
         """"""
@@ -144,27 +120,7 @@ class BaseTransformerModule(pl.LightningModule):
 
     def _set_objective(self) -> None:
         """"""
-        objective = self.config.get("objective", "ce")
-        self.smoothing = self.config.get("smoothing", 0.0)
-        self.class_weights = self.config.get("class_weights", [1.0] * self.num_labels)
-
-        if objective == "lsl" and self.smoothing == 0.0:
-            logging.warning(
-                "You are using label smoothing and the smoothing parameter"
-                "is set to 0.0."
-            )
-        elif objective == "weighted" and all([w == 1.0 for w in self.class_weights]):
-            logging.warning(
-                "You are using a weighted CrossEntropy but the class"
-                "weights are all equal to 1.0."
-            )
-        self.objective = {
-            "ce": CrossEntropyLoss(),
-            "lsl": LabelSmoothingLoss(
-                nb_classes=self.num_labels, smoothing=self.smoothing
-            ),
-            "weighted": CrossEntropyLoss(weight=torch.Tensor(self.class_weights)),
-        }[objective]
+        raise NotImplementedError()
 
     @staticmethod
     def _sanity_checks(training_config: DictConfig) -> None:
@@ -179,11 +135,6 @@ class BaseTransformerModule(pl.LightningModule):
         assert (
             training_config.logging_steps > training_config.accumulation_steps
         ), "'logging_steps' should be greater than 'accumulation_steps'"
-
-        if training_config.get("scorer_type") == "loose":
-            assert (
-                "loose_classes" in training_config.keys()
-            ), "To use a 'LooseScorer' you need to set a 'loose_classes' parameter in your training config."
 
     def _get_optimizer_parameters(self) -> List[Dict]:
         """
@@ -287,6 +238,130 @@ class BaseTransformerModule(pl.LightningModule):
             ]
         return optimizer_grouped_parameters
 
+    def _set_scorers(self, *args, **kwargs) -> None:
+        """"""
+        raise NotImplementedError()
+
+    def prune_heads(self, heads_to_prune: Dict[str, List[str]]) -> None:
+        """"""
+        raise NotImplementedError()
+
+    def freeze_encoder(self) -> None:
+        """Freeze encoder layers"""
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+    def unfreeze_encoder(self) -> None:
+        """Unfreeze encoder layers"""
+        for param in self.encoder.parameters():
+            param.requires_grad = True
+
+    def loss(self, *args, **kwargs) -> Loss:
+        """"""
+        raise NotImplementedError()
+
+    def log_eval_report(self, *args) -> None:
+        """
+        Method that logs an evaluation report.
+
+        It uses the evaluation scorer to log all the available losses and metrics
+        """
+        table = self.valid_scorer.get_table()
+        self.logger.experiment.add_text("eval/report", table)
+
+        logging_loss = {
+            key: torch.stack(val).mean() for key, val in self.valid_scorer.losses.items()
+        }
+        self.log_dict({f"eval/loss_{key}": val for key, val in logging_loss.items()})
+
+        eval_report = self.valid_scorer.to_dict()
+        for metric, value in eval_report.items():
+            if isinstance(value, dict):
+                self.log_dict({f"eval/{metric}/{key}": v for key, v in value.items()})
+            elif not isinstance(value, list) and not isinstance(value, np.ndarray):
+                self.log("eval/{}".format(metric), value)
+
+
+class BaseSequenceClassificationTransformerModule(BaseTransformerModule):
+    """
+    Base class to extend for all modules.
+
+    Args:
+        training_config (DictConfig):
+            training configuration
+        num_labels (int):
+            number of labels
+        pretrained_model (str):
+            name of the pretrained Transformer model to use
+    """
+
+    def __init__(
+        self,
+        training_config: DictConfig,
+        pretrained_model: str,
+        num_labels: int,
+        **kwargs,
+    ):
+        super().__init__(training_config, pretrained_model, **kwargs)
+        self._sanity_checks(training_config)
+        self.num_labels = num_labels
+        self.model_config = AutoConfig.from_pretrained(
+            pretrained_model, num_labels=num_labels
+        )
+
+        self._set_scorers(kwargs.get("scorer_type", "regular"))
+        self._set_objective()
+
+    def on_validation_epoch_end(self):
+        """"""
+        if not self.trainer.sanity_checking:
+            all_logits = torch.cat(
+                [pred["logits"] for pred in self.validation_step_outputs]
+            )
+            all_probs = F.softmax(all_logits, dim=-1)
+            labels_probs = all_probs.numpy()
+            self.log_eval_report(labels_probs)
+
+        self.valid_scorer.reset()
+        self.validation_step_outputs.clear()
+
+    def _sanity_checks(self, training_config: DictConfig) -> None:
+        """
+        Args:
+            training_config (DictConfig):
+                training configuration
+        """
+        super()._sanity_checks(training_config)
+
+        if training_config.get("scorer_type") == "loose":
+            assert (
+                "loose_classes" in training_config.keys()
+            ), "To use a 'LooseScorer' you need to set a 'loose_classes' parameter in your training config."
+
+    def _set_objective(self) -> None:
+        """"""
+        objective = self.config.get("objective", "ce")
+        self.smoothing = self.config.get("smoothing", 0.0)
+        self.class_weights = self.config.get("class_weights", [1.0] * self.num_labels)
+
+        if objective == "lsl" and self.smoothing == 0.0:
+            logging.warning(
+                "You are using label smoothing and the smoothing parameter"
+                "is set to 0.0."
+            )
+        elif objective == "weighted" and all([w == 1.0 for w in self.class_weights]):
+            logging.warning(
+                "You are using a weighted CrossEntropy but the class"
+                "weights are all equal to 1.0."
+            )
+        self.objective = {
+            "ce": CrossEntropyLoss(),
+            "lsl": LabelSmoothingLoss(
+                nb_classes=self.num_labels, smoothing=self.smoothing
+            ),
+            "weighted": CrossEntropyLoss(weight=torch.Tensor(self.class_weights)),
+        }[objective]
+
     def _set_scorers(self, scorer_type: str) -> None:
         """
         Method to set the scorers to use to evaluate the model.
@@ -315,15 +390,17 @@ class BaseTransformerModule(pl.LightningModule):
         for layer, heads in heads_to_prune.items():
             self.encoder.encoder.layer[layer].attention.prune_heads(heads)
 
-    def freeze_encoder(self) -> None:
-        """Freeze encoder layers"""
-        for param in self.encoder.parameters():
-            param.requires_grad = False
+    def loss(self, labels: torch.Tensor, logits: torch.Tensor, *args, **kwargs) -> Loss:
+        """
+        Method called for loss computation
 
-    def unfreeze_encoder(self) -> None:
-        """Unfreeze encoder layers"""
-        for param in self.encoder.parameters():
-            param.requires_grad = True
+        Args:
+            logits (torch.Tensor):
+                predicted logits
+            labels (torch.Tensor):
+                ground truth labels
+        """
+        return self.objective(logits.view(-1, self.num_labels), labels.view(-1).long())
 
     def loss(self, labels: torch.Tensor, logits: torch.Tensor, *args, **kwargs) -> Loss:
         """
@@ -348,20 +425,7 @@ class BaseTransformerModule(pl.LightningModule):
             probs (np.array):
                 predicted probabilities
         """
-        table = self.valid_scorer.get_table()
-        self.logger.experiment.add_text("eval/report", table)
-
-        logging_loss = {
-            key: torch.stack(val).mean() for key, val in self.valid_scorer.losses.items()
-        }
-        self.log_dict({f"eval/loss_{key}": val for key, val in logging_loss.items()})
-
-        eval_report = self.valid_scorer.to_dict()
-        for metric, value in eval_report.items():
-            if isinstance(value, dict):
-                self.log_dict({f"eval/{metric}/{key}": v for key, v in value.items()})
-            elif not isinstance(value, list) and not isinstance(value, np.ndarray):
-                self.log("eval/{}".format(metric), value)
+        super().log_eval_report()
 
         for i in range(probs.shape[1]):
             fig = plt.figure(figsize=(15, 15))
