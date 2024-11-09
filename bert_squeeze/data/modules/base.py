a49991b224d95b50d1cb3391d3b2f90c970cb161
JulesBelveze <jules.belveze@hotmail.fr>
2024-11-09 08:42:06 +0100

    [bert_squeeze/data] - refactor: improve readability of dataset loading
     - Refactor the line that loads the dataset to span multiple lines for better code readability
     - Maintain functionality of trusting remote dataset code by setting `trust_remote_code=True` in a more readable format

--- bert_squeeze/data/modules/base.py
+++ bert_squeeze/data/modules/base.py
@@ -27,7 +27,9 @@ class BaseDataModule(pl.LightningDataModule):
         Returns:
             None
         """
-        self.dataset = datasets.load_dataset(self.dataset_config.path, trust_remote_code=True)
+        self.dataset = datasets.load_dataset(
+            self.dataset_config.path, trust_remote_code=True
+        )
 
         if "percent" not in self.dataset_config:
             return
